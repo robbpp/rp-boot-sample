80e28a64800d0769b4ed86d0ed5f1ce06560b057
roberto.palacios <rpalacios@cgtscorp.com>
2018-07-11 16:01:08 -0400

    Adding Boot Simple Run.

---
+++ boot-simple-run/src/main/java/io/rptest/bootsimplerun/BootSimpleRunApp.java
@@ -0,0 +1,12 @@
+package io.rptest.bootsimplerun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSimpleRunApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSimpleRunApp.class, args);
+	}
+}
